5b44297bd6ca8ae21698e517851bc30b47fb760c
jq.run <dev@jq.run>
2021-04-07 15:59:50 -0400

    Pausing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
-import { FancyButton } from 'repro-ui-lib';
+import { FancyButton, FancyProvider } from 'repro-ui-lib';
 
 const App: FunctionComponent = () => {
   return (
-    // <FancyProvider>
-    <>
+    <FancyProvider>
       <h1>Hello world!</h1>
       <FancyButton>Test</FancyButton>
-    </>
-    // </FancyProvider>
+    </FancyProvider>
   );
 };
 
